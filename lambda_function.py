4b30a0ce4cbfc201110431cc1f3f82e3687f7fb0
Marc Brophy <aldrichbrophy@gmail.com>
2023-11-05 18:22:42 -0800

    added comprehend
    Signed-off-by: Marc Brophy <aldrichbrophy@gmail.com>

--- lambda_function.py
+++ lambda_function.py
@@ -6,7 +6,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 getMethod = "GET"
-healthPath = "/health"
+healthPath = "/comprehend/health"
 
 #main handler
 def lambda_handler(event, context):
