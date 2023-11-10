d44844a5be89da8ad08156dfbe9262a819a55cd4
Sai Supraj Reddy <supraj_reddy@outlook.com>
2023-11-10 12:47:12 -0800

    Added function for dominant language detection and sentiment detection

--- lambda_function.py
+++ lambda_function.py
@@ -1,36 +1,65 @@
 import logging
 import json
+import boto3
 from customEncoder import CustomEncoder
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 getMethod = "GET"
-healthPath = "/comprehend/health"
 
-#main handler
-def lambda_handler(event, context):
+healthPath = "/comprehend/health"
+getPath = "/comprehend"
+
+comprehend_client = boto3.client('comprehend')
+
+
+# main handler
+def lambda_handler(event):
     logger.info(event)
     httpMethod = event['httpMethod']
+    text = event['Text']
     path = event['path']
 
     if httpMethod == getMethod and path == healthPath:
         response = buildResponse(200)
+    elif httpMethod == getMethod and path == getPath:
+        # Analyze text using AWS Comprehend
+        analysis_result = analyze_text(text)
+
+        # Build a response with the analysis result
+        response = buildResponse(200, analysis_result)
     else:
         response = buildResponse(404, 'Not Found')
-    
+
     return response
 
+
+# Analyze text and derive sentiment using AWS Comprehend
+def analyze_text(text):
+    language_response = comprehend_client.detect_dominant_language(Text=text)
+    detected_language = language_response['Languages'][0]['LanguageCode']
+
+    sentiment_response = comprehend_client.detect_sentiment(Text=text, LanguageCode=detected_language)
+
+    result = {
+        'DetectedLanguage': detected_language,
+        'Sentiment': sentiment_response['Sentiment'],
+        'SentimentScore': sentiment_response['SentimentScore']
+    }
+
+    return result
+
+
 # Response builder.
-def buildResponse(statusCode, body = None):
+def buildResponse(statusCode, body=None):
     response = {
         'statusCode': statusCode,
         'headers': {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'
-        },
-        'body': "Hello! Just health check"
+        }
     }
     if body is not None:
-        response['body'] = json.dumps(body, cls = CustomEncoder)
+        response['body'] = json.dumps(body, cls=CustomEncoder)
     return response
